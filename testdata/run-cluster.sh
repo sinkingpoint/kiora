ddf2d7fde505bf59035732c6665359bef1933a33
sinkingpoint <colin@quirl.co.nz>
2022-12-19 19:12:12 +1300

    Add cluster estabilishing in run-cluster
    This adds to the cluster startup script a step that adds in 2 and 3 into
    the cluster with the /add_member call.
    
    Signed-off-by: sinkingpoint <colin@quirl.co.nz>

--- testdata/run-cluster.sh
+++ testdata/run-cluster.sh
@@ -12,7 +12,17 @@ function on_exit() {
 }
 
 mkdir -p artifacts/logs
+
+echo 'Starting Kiora'
 ./artifacts/kiora -c ./testdata/kiora.dot --raft.bootstrap --raft.data-dir artifacts/kiora-raft-data-1 --raft.local-id 1 --web.listen-url localhost:4278 --raft.listen-url localhost:4281 > artifacts/logs/kiora-1.log 2>&1 &
 ./artifacts/kiora -c ./testdata/kiora.dot --raft.data-dir artifacts/kiora-raft-data-2 --raft.local-id 2 --web.listen-url localhost:4279 --raft.listen-url localhost:4282 > artifacts/logs/kiora-2.log 2>&1 &
 ./artifacts/kiora -c ./testdata/kiora.dot --raft.data-dir artifacts/kiora-raft-data-3 --raft.local-id 3 --web.listen-url localhost:4280 --raft.listen-url localhost:4283 > artifacts/logs/kiora-3.log 2>&1 &
+
+echo 'Establishing the Cluster'
+sleep 2
+curl -XPOST -d '{"id":"2","address":"localhost:4282"}' localhost:4278/admin/raft/add_member
+curl -XPOST -d '{"id":"3","address":"localhost:4283"}' localhost:4278/admin/raft/add_member
+
+curl localhost:4278/admin/raft/status
+
 read -r -d '' _
