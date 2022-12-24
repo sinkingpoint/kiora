2737fa2f088b4c5819d727465ecf415801efcd05
sinkingpoint <colin@quirl.co.nz>
2022-12-24 15:25:27 +1300

    Split the DB interface into seperate reader and writers
    Signed-off-by: sinkingpoint <colin@quirl.co.nz>

--- lib/kiora/kioradb/db.go
+++ lib/kiora/kioradb/db.go
@@ -6,14 +6,30 @@ import (
 	"github.com/sinkingpoint/kiora/lib/kiora/model"
 )
 
-// DB defines an interface that is able to process alerts, silences etc and store them (for some definition of store).
-type DB interface {
-	// ProcessAlerts takes alerts and processes them, adding new ones and resolving old ones.
-	ProcessAlerts(ctx context.Context, alerts ...model.Alert) error
-
+// ModelReader defines an interface that can be used to get various models out of an underlying system.
+type ModelReader interface {
 	// GetAlerts gets all the alerts currently in the database.
 	GetAlerts(ctx context.Context) ([]model.Alert, error)
+}
 
+type AlertProcessor interface {
+	// ProcessAlerts takes alerts and processes them, adding new ones and resolving old ones.
+	ProcessAlerts(ctx context.Context, alerts ...model.Alert) error
+}
+
+type SilenceProcessor interface {
 	// ProcessSilences takes silences and processes them.
 	ProcessSilences(ctx context.Context, silences ...model.Silence) error
+}
+
+// ModelWriter defines an interface that takes various models and processes them, with no way to get them back out.
+type ModelWriter interface {
+	AlertProcessor
+	SilenceProcessor
+}
+
+// DB defines an interface that is able to process alerts, silences etc and store them (for some definition of store).
+type DB interface {
+	ModelReader
+	ModelWriter
 }
