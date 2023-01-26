89b20cd20840eda10621572a69773933c1cd20fa
sinkingpoint <colin@quirl.co.nz>
2023-01-26 11:14:51 +1100

    Add RegexFilter
    This adds in a filter that allows matching on label values
    
    Signed-off-by: sinkingpoint <colin@quirl.co.nz>

--- cmd/kiora/config/links.go
+++ cmd/kiora/config/links.go
@@ -1,6 +1,11 @@
 package config
 
-import "github.com/sinkingpoint/kiora/lib/kiora/model"
+import (
+	"errors"
+	"regexp"
+
+	"github.com/sinkingpoint/kiora/lib/kiora/model"
+)
 
 type Filter interface {
 	Type() string
@@ -14,8 +19,50 @@ type Link struct {
 
 type FilterConstructor = func(n edge) (Filter, error)
 
-var filterRegistry = map[string]FilterConstructor{}
+var filterRegistry = map[string]FilterConstructor{
+	"regex": NewRegexFilter,
+}
 
 func LookupFilter(name string) FilterConstructor {
 	return filterRegistry[name]
+}
+
+// RegexFilter is a filter that matches if the given alert a) has the given label and b) that label matches a regex.
+type RegexFilter struct {
+	Label string
+	Regex *regexp.Regexp
+}
+
+func NewRegexFilter(n edge) (Filter, error) {
+	field := n.attrs["field"]
+	if field == "" {
+		return nil, errors.New("expected `field` in regex filter")
+	}
+
+	regexStr := n.attrs["regex"]
+	if regexStr == "" {
+		return nil, errors.New("expected `regex` in regex filter")
+	}
+
+	regex, err := regexp.Compile(regexStr)
+	if err == nil {
+		return nil, err
+	}
+
+	return &RegexFilter{
+		Label: field,
+		Regex: regex,
+	}, nil
+}
+
+func (r *RegexFilter) Type() string {
+	return "regex"
+}
+
+func (r *RegexFilter) FilterAlert(a *model.Alert) bool {
+	if label, ok := a.Labels[r.Label]; ok {
+		return r.Regex.MatchString(label)
+	}
+
+	return false
 }
---
+++ cmd/kiora/config/links_test.go
@@ -0,0 +1,67 @@
+package config_test
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/sinkingpoint/kiora/cmd/kiora/config"
+	"github.com/sinkingpoint/kiora/lib/kiora/model"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRegexFilter(t *testing.T) {
+	tests := []struct {
+		Name        string
+		Label       string
+		Regex       string
+		Alert       model.Alert
+		ShouldMatch bool
+	}{
+		{
+			Name:  "standard match",
+			Label: "test",
+			Regex: "test",
+			Alert: model.Alert{
+				Labels: model.Labels{
+					"test": "test",
+				},
+			},
+			ShouldMatch: true,
+		},
+		{
+			Name:  "non existant label",
+			Label: "some_weird_non_existant_label",
+			Regex: "test",
+			Alert: model.Alert{
+				Labels: model.Labels{
+					"test": "test",
+				},
+			},
+			ShouldMatch: false,
+		},
+		{
+			Name:  "non regex match",
+			Label: "test",
+			Regex: "^test$",
+			Alert: model.Alert{
+				Labels: model.Labels{
+					"test": "not test",
+				},
+			},
+			ShouldMatch: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			regex := regexp.MustCompile(tt.Regex)
+			match := config.RegexFilter{
+				Label: tt.Label,
+				Regex: regex,
+			}
+
+			matches := match.FilterAlert(&tt.Alert)
+			assert.Equal(t, tt.ShouldMatch, matches)
+		})
+	}
+}
