99561c1487348e7e1f414fd2bbf68650927651a8
sinkingpoint <colin@quirl.co.nz>
2023-03-18 18:21:25 +1100

    Add Timeout Service
    This adds in a service that is responsible for marking alerts as timed out
    if they go beyond their timed out time.
    
    Signed-off-by: sinkingpoint <colin@quirl.co.nz>

---
+++ internal/services/timeout/service.go
@@ -0,0 +1,52 @@
+package timeout
+
+import (
+	"context"
+	"time"
+
+	"github.com/sinkingpoint/kiora/internal/services"
+	"github.com/sinkingpoint/kiora/lib/kiora/kioradb/query"
+	"github.com/sinkingpoint/kiora/lib/kiora/model"
+)
+
+type TimeoutService struct {
+	bus services.Bus
+}
+
+func NewTimeoutService(bus services.Bus) *TimeoutService {
+	return &TimeoutService{
+		bus: bus,
+	}
+}
+
+func (t *TimeoutService) Name() string {
+	return "timeout"
+}
+
+func (t *TimeoutService) Run(ctx context.Context) error {
+	ticker := time.NewTicker(1 * time.Second)
+
+	for {
+		select {
+		case <-ticker.C:
+			t.timeoutAlerts(ctx)
+		case <-ctx.Done():
+			return nil
+		}
+	}
+}
+
+func (t *TimeoutService) timeoutAlerts(ctx context.Context) {
+	query := query.Status(model.AlertStatusFiring)
+	changed := []model.Alert{}
+	for _, a := range t.bus.DB().QueryAlerts(ctx, query) {
+		if a.TimeOutDeadline.Before(time.Now()) {
+			a.Status = model.AlertStatusTimedOut
+			changed = append(changed, a)
+		}
+	}
+
+	if err := t.bus.Broadcaster().BroadcastAlerts(ctx, changed...); err != nil {
+		t.bus.Logger("timeout").Warn().Err(err).Msg("failed to broadcast alerts")
+	}
+}
