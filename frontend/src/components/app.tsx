b82e72125e5f963cc55b628f10806503b4a0648d
sinkingpoint <colin@quirl.co.nz>
2023-03-23 09:35:47 +1100

    Add initial home component
    Signed-off-by: sinkingpoint <colin@quirl.co.nz>

--- frontend/src/components/app.tsx
+++ frontend/src/components/app.tsx
@@ -1,5 +1,15 @@
 import { h } from "preact";
+import Router, { Route } from "preact-router";
+import Home from "../routes/home";
 
-const App = () => <div id="app" />;
+const App = () => {
+  return (
+    <div id="app">
+      <Router>
+        <Route path="/" component={Home} />
+      </Router>
+    </div>
+  );
+};
 
 export default App;
---
+++ frontend/src/routes/home/index.tsx
@@ -0,0 +1,11 @@
+import { h } from "preact";
+
+const Home = () => {
+  return (
+    <div>
+      <h3>Kiora</h3>
+    </div>
+  );
+};
+
+export default Home;
