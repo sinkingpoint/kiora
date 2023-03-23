67bce163b67d447126e33b41f3e18dcee23cbdbb
sinkingpoint <colin@quirl.co.nz>
2023-03-24 09:15:29 +1100

    Initial Header Component
    This adds in a header component that will be used on all pages to provide
    navigation.
    
    Signed-off-by: sinkingpoint <colin@quirl.co.nz>

--- frontend/src/components/app.tsx
+++ frontend/src/components/app.tsx
@@ -1,13 +1,17 @@
 import { h } from "preact";
 import Router, { Route } from "preact-router";
 import Home from "../routes/home";
+import Header from "./header";
 
 const App = () => {
   return (
     <div id="app">
-      <Router>
-        <Route path="/" component={Home} />
-      </Router>
+      <Header />
+      <main>
+        <Router>
+          <Route path="/" component={Home} />
+        </Router>
+      </main>
     </div>
   );
 };
--- frontend/src/routes/home/index.tsx
+++ frontend/src/routes/home/index.tsx
@@ -4,7 +4,6 @@ import AlertView from "../../components/alertview";
 const Home = () => {
   return (
     <div>
-      <h3>Kiora</h3>
       <AlertView />
     </div>
   );
