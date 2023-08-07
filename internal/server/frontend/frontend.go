45dea01a4d8b73354f2cdfbb3f2e40d783ac5edc
sinkingpoint <colin@quirl.co.nz>
2023-08-07 10:49:33 +1000

    Fix frontend serving
    The frontend was being served based entirely from the filesystem, which
    fails for anything not the index, using the preact router. This change
    adds a catch-all route to the frontend, which will serve the index.html
    for any route that isn't a file.
    
    Signed-off-by: sinkingpoint <colin@quirl.co.nz>

--- internal/server/frontend/frontend.go
+++ internal/server/frontend/frontend.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"io/fs"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -18,5 +19,21 @@ func Register(router *mux.Router) {
 		panic("BUG: failed to embed frontend assets: " + err.Error())
 	}
 
-	router.PathPrefix("/").Handler(http.FileServer(http.FS(sub)))
+	serveReactApp := func(w http.ResponseWriter, r *http.Request) {
+		idx, err := fs.ReadFile(sub, "index.html")
+		if err != nil {
+			panic("BUG: failed to read embedded index.html: " + err.Error())
+		}
+
+		w.Header().Set("Content-Type", "text/html")
+		w.Write(idx)
+	}
+
+	router.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.Contains(r.URL.Path, ".") {
+			http.FileServer(http.FS(sub)).ServeHTTP(w, r)
+		} else {
+			serveReactApp(w, r)
+		}
+	})
 }
